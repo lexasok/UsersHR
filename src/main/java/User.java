2362254acb7cbb06d8612b0ca8fc12d81f2e6b08
lexa <sokolov@ozero.net>
2018-05-17 23:21:54 +0300

    Constructor initialized

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -5,7 +5,14 @@ public class User {
     String username;
     String password;
     int age;
-    Set<Integer> orderIds;
+    Set<Integer> orderIDs;
+
+    public User(String customUsername, String customPassword, int customAge, Set<Integer> orderIDs) {
+        this.username = customUsername;
+        this.password = customPassword;
+        this.age = customAge;
+        this.orderIDs = orderIDs;
+    }
 
     public static void main(String[] args) {
 
