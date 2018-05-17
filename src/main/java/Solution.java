05b2626bf26a3a0f2acf13c36976cb99938c8f47
lexa <sokolov@ozero.net>
2018-05-18 00:14:10 +0300

    Soulution implemented

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Solution {
 
@@ -7,11 +9,19 @@ public class Solution {
     public static void main(String[] args) {
         int n = scan.nextInt();
         scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        String regEx = "[a-zA-Z0-9_]+@gmail.com";
+        Pattern pattern = Pattern.compile(regEx);
+
 
         for (int i = 0; i < n; i++) {
             String[] firstNameEmailID = scan.nextLine().split(" ");
             String firstName = firstNameEmailID[0];
             String emailId = firstNameEmailID[1];
+
+            Matcher matcher = pattern.matcher(emailId);
+            if (matcher.matches()) {
+                System.out.println(firstName);
+            }
         }
 
         scan.close();
