5fb957fd7dc21c80615aefa3a37a00c04d7d9761
lexa <sokolov@ozero.net>
2018-05-17 23:19:06 +0300

    Fields added

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,4 +1,12 @@
+import java.util.Set;
+
 public class User {
+
+    String username;
+    String password;
+    int age;
+    Set<Integer> orderIds;
+
     public static void main(String[] args) {
 
     }
