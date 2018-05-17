7fdadf9317f94b0442ef6461399c2a3fabd80273
lexa <sokolov@ozero.net>
2018-05-17 23:58:26 +0300

    Base program initialize

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -5,6 +5,15 @@ public class Solution {
     private static final Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int n = scan.nextInt();
+        scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
+        for (int i = 0; i < n; i++) {
+            String[] firstNameEmailID = scan.nextLine().split(" ");
+            String firstName = firstNameEmailID[0];
+            String emailId = firstNameEmailID[1];
+        }
+
+        scan.close();
     }
 }
